ceb724154d53c30709a49fffbfc82ebc44d0e90b
AA <asheamar@gmail.com>
2023-02-11 18:12:22 -0500

    ADAPTERS-8 NUnit project initialized

---
+++ test/Dark Adapters Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Dark_Adapters_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
