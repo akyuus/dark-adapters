2e08db0737d234a3ce3647e821eeeafc3589f077
AA <asheamar@gmail.com>
2023-08-12 16:55:02 -0400

    ADAPTERS-16 collisions optimized, cleaned up some other code too

--- src/model/cell.rs
+++ src/model/cell.rs
@@ -1,11 +1,13 @@
 use std::f32::consts::PI;
 
 use bevy::prelude::{
-    BuildChildren, Bundle, Commands, Component, Quat, ResMut, Resource, SpatialBundle, Transform,
-    Vec3,
+    BuildChildren, Bundle, Commands, Component, Entity, Quat, ResMut, Resource, SpatialBundle,
+    Transform, Vec3,
 };
 use bevy::utils::HashMap;
+use serde::{Deserialize, Serialize};
 
+use crate::model::grid::DungeonTileLookup;
 use crate::model::tile::{PurpleTexture, PurpleTileTextureMap, Tile};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -193,35 +195,83 @@ pub fn initialize_preset_map(
     //endregion
 }
 
-pub enum TileDirection {
-    Left,
-    Forward,
-    Right,
-    Back,
-    Top,
-    Bottom,
+#[derive(Hash, Clone, Copy, Eq, PartialEq, Serialize, Deserialize, Component, Debug)]
+pub enum GridDirection {
+    Left = 0,
+    Forward = 1,
+    Right = 2,
+    Back = 3,
+    Top = 4,
+    Bottom = 5,
 }
 
-impl TileDirection {
-    fn get_tile_transform(direction: TileDirection) -> Transform {
+impl GridDirection {
+    fn get_tile_transform(direction: GridDirection) -> Transform {
         // these tiny offsets are here to prevent z-fighting
         match direction {
-            TileDirection::Left => Transform::from_xyz(-0.5000001, 1.0, 0.0)
+            GridDirection::Left => Transform::from_xyz(-0.5000001, 1.0, 0.0)
                 .with_rotation(Quat::from_rotation_y(-PI / 2.0)),
-            TileDirection::Forward => {
+            GridDirection::Forward => {
                 Transform::from_xyz(0.0, 1.0, -0.5000001).with_rotation(Quat::from_rotation_z(PI))
             }
-            TileDirection::Right => Transform::from_xyz(0.5000001, 1.0, 0.0)
+            GridDirection::Right => Transform::from_xyz(0.5000001, 1.0, 0.0)
                 .with_rotation(Quat::from_rotation_y(PI / 2.0)),
-            TileDirection::Back => Transform::from_xyz(0.0, 1.0, 0.5000001),
-            TileDirection::Top => Transform::from_xyz(0.0, 1.5000001, 0.0)
+            GridDirection::Back => Transform::from_xyz(0.0, 1.0, 0.5000001),
+            GridDirection::Top => Transform::from_xyz(0.0, 1.5000001, 0.0)
                 .with_rotation(Quat::from_rotation_x(PI / 2.0)),
-            TileDirection::Bottom => Transform::from_xyz(0.0, 0.5000001, 0.0)
+            GridDirection::Bottom => Transform::from_xyz(0.0, 0.5000001, 0.0)
                 .with_rotation(Quat::from_rotation_x(-PI / 2.0)),
         }
     }
     fn set_tile_transform(tile: &mut Tile, transform: Transform) {
         tile.set_tile_transform(transform);
+    }
+
+    pub fn get_rotated_direction(self, rotate_dir: GridDirection) -> Self {
+        match self {
+            GridDirection::Left
+            | GridDirection::Forward
+            | GridDirection::Right
+            | GridDirection::Back => match rotate_dir {
+                GridDirection::Left => {
+                    let num_self = self as i8;
+                    let new_dir: GridDirection = ((num_self + 3) % 4).try_into().unwrap();
+                    new_dir
+                }
+                GridDirection::Right => {
+                    let num_self = self as i8;
+                    let new_dir: GridDirection = ((num_self + 1) % 4).try_into().unwrap();
+                    new_dir
+                }
+                _ => self,
+            },
+            _ => self,
+        }
+    }
+
+    pub fn get_inverse_direction(self) -> Self {
+        match self {
+            GridDirection::Left => GridDirection::Right,
+            GridDirection::Forward => GridDirection::Back,
+            GridDirection::Right => GridDirection::Left,
+            GridDirection::Back => GridDirection::Forward,
+            GridDirection::Top => GridDirection::Bottom,
+            GridDirection::Bottom => GridDirection::Top,
+        }
+    }
+}
+
+impl From<i8> for GridDirection {
+    fn from(value: i8) -> Self {
+        match value {
+            0 => GridDirection::Left,
+            1 => GridDirection::Forward,
+            2 => GridDirection::Right,
+            3 => GridDirection::Back,
+            4 => GridDirection::Top,
+            5 => GridDirection::Bottom,
+            _ => GridDirection::Forward, // this shouldn't ever happen
+        }
     }
 }
 
@@ -230,7 +280,7 @@ pub enum DungeonCellType {
     Basic,
 }
 
-#[derive(Component, Copy, Clone)]
+#[derive(Component, Copy, Clone, Debug)]
 pub struct GridPosition {
     pub row: usize,
     pub col: usize,
@@ -243,6 +293,28 @@ impl GridPosition {
 
     pub fn to_cell_vec3(self) -> Vec3 {
         Vec3::new(self.col as f32, 0.0, self.row as f32)
+    }
+
+    pub fn translated(self, direction: GridDirection) -> Self {
+        match direction {
+            GridDirection::Left => GridPosition {
+                row: self.row,
+                col: self.col - 1,
+            },
+            GridDirection::Forward => GridPosition {
+                row: self.row - 1,
+                col: self.col,
+            },
+            GridDirection::Right => GridPosition {
+                row: self.row,
+                col: self.col + 1,
+            },
+            GridDirection::Back => GridPosition {
+                row: self.row + 1,
+                col: self.col,
+            },
+            _ => self,
+        }
     }
 }
 
@@ -310,29 +382,29 @@ impl TileBundle {
         mut top: Tile,
         mut bottom: Tile,
     ) -> Self {
-        TileDirection::set_tile_transform(
+        GridDirection::set_tile_transform(
             &mut left,
-            TileDirection::get_tile_transform(TileDirection::Left),
+            GridDirection::get_tile_transform(GridDirection::Left),
         );
-        TileDirection::set_tile_transform(
+        GridDirection::set_tile_transform(
             &mut forward,
-            TileDirection::get_tile_transform(TileDirection::Forward),
+            GridDirection::get_tile_transform(GridDirection::Forward),
         );
-        TileDirection::set_tile_transform(
+        GridDirection::set_tile_transform(
             &mut right,
-            TileDirection::get_tile_transform(TileDirection::Right),
+            GridDirection::get_tile_transform(GridDirection::Right),
         );
-        TileDirection::set_tile_transform(
+        GridDirection::set_tile_transform(
             &mut back,
-            TileDirection::get_tile_transform(TileDirection::Back),
+            GridDirection::get_tile_transform(GridDirection::Back),
         );
-        TileDirection::set_tile_transform(
+        GridDirection::set_tile_transform(
             &mut top,
-            TileDirection::get_tile_transform(TileDirection::Top),
+            GridDirection::get_tile_transform(GridDirection::Top),
         );
-        TileDirection::set_tile_transform(
+        GridDirection::set_tile_transform(
             &mut bottom,
-            TileDirection::get_tile_transform(TileDirection::Bottom),
+            GridDirection::get_tile_transform(GridDirection::Bottom),
         );
 
         TileBundle {
@@ -346,18 +418,39 @@ impl TileBundle {
     }
 }
 
-pub fn spawn_dungeon_cell(cell: DungeonCell, commands: &mut Commands) -> DungeonCell {
+pub fn spawn_dungeon_cell(
+    mut cell: DungeonCell,
+    grid_position: GridPosition,
+    commands: &mut Commands,
+    dungeon_tile_lookup: &mut ResMut<DungeonTileLookup>,
+) {
     // TODO: ADAPTERS-16
-    let cloned_cell = cell.clone();
+
+    let mut insert_into_lookup_closure = |direction: GridDirection, entity: Entity| {
+        dungeon_tile_lookup.insert_tile(grid_position, direction, entity);
+    };
+
+    cell.set_position(grid_position);
     let left = commands.spawn(cell.tile_bundle.left).id();
+    insert_into_lookup_closure(GridDirection::Left, left);
+
     let forward = commands.spawn(cell.tile_bundle.forward).id();
+    insert_into_lookup_closure(GridDirection::Forward, forward);
+
     let right = commands.spawn(cell.tile_bundle.right).id();
+    insert_into_lookup_closure(GridDirection::Right, right);
+
     let back = commands.spawn(cell.tile_bundle.back).id();
+    insert_into_lookup_closure(GridDirection::Back, back);
+
     let top = commands.spawn(cell.tile_bundle.top).id();
+    insert_into_lookup_closure(GridDirection::Top, top);
+
     let bottom = commands.spawn(cell.tile_bundle.bottom).id();
+    insert_into_lookup_closure(GridDirection::Bottom, bottom);
+
     let cell_id = commands.spawn((cell.cell_type, cell.spatial_bundle)).id();
     commands
         .entity(cell_id)
         .push_children(&[left, forward, right, back, top, bottom]);
-    cloned_cell
 }
